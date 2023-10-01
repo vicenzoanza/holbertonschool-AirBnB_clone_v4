a09fc69a10d30bdadaaf846d5f495a34a0af0463
alejandr088 <alejandrivello@gmail.com>
2023-09-30 17:25:41 -0700

    task 4, fixed folder name, need more test

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    function updateApiStatus() {
+        $.get('http://0.0.0.0:5001/api/v1/status/', function(data) {
+            if (data.status === 'OK') {
+                $('div#api_status').addClass('available');
+            } else {
+                $('div#api_status').removeClass('available');
+            }
+        });
+    }
+
+    function searchPlaces() {
+        const searchParams = {};
+
+        const states = $('input[name="states"]').val();
+        if (states) {
+            searchParams.states = states.split(',').map(state => state.trim());
+        }
+
+        const cities = $('input[name="cities"]').val();
+        if (cities) {
+            searchParams.cities = cities.split(',').map(city => city.trim());
+        }
+
+        const amenities = $('input[name="amenities"]').val();
+        if (amenities) {
+            searchParams.amenities = amenities.split(',').map(amenity => amenity.trim());
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://0.0.0.0:5001/api/v1/places_search/',
+            contentType: 'application/json',
+            data: JSON.stringify(searchParams),
+            success: function(data) {
+                console.log(data);
+            },
+            error: function(error) {
+                console.error(error);
+            }
+        });
+    }
+
+    updateApiStatus();
+
+    $('button').click(function() {
+        searchPlaces();
+    });
+});
